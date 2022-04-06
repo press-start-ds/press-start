401b471a612c05841bd37bd69bf659b4fe21221e
Luiz Otavio Vilas Boas Oliveira <luizvbo@gmail.com>
2022-04-06 07:42:48 +0200

    ENH: Update to Kedro 18

--- src/press_start/pipelines/data_split/nodes.py
+++ src/press_start/pipelines/data_split/nodes.py
@@ -1,31 +1,35 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from typing import Dict
+from typing import Dict, Union
 
 from press_start.utils import GeneralParams
 
 
 def data_split(
-    df: pd.DataFrame, params: Dict[str, Dict], general_params_in: Dict[str, Dict]
+    df: pd.DataFrame,
+    params: Dict[str, Union[int, float]],
+    general_params_dict: Dict[str, Dict],
 ):
-    gen_params = GeneralParams(general_params_in)
-    if 'dev_size' in params:
-        test_size = int(params['dev_size'])
-        assert (test_size >= 0) and (test_size < df.shape[0] - 1), (
-            "data_split.dev_size must be in [0, n_rows - 1]"
-        )
+    general_params = GeneralParams(general_params_dict)
+    test_size: Union[int, float]
+    if "dev_size" in params:
+        test_size = int(params["dev_size"])
+        assert (test_size >= 0) and (
+            test_size < df.shape[0] - 1
+        ), "data_split.dev_size must be in [0, n_rows - 1]"
     else:
-        test_size = params.get('dev_fraction', 0)
-        assert (test_size >= 0) and (test_size < 1), (
-            "data_split.dev_fraction must be in [0.0, 1.0)."
-        )
-    if params.get('stratify', False):
-        stratify_data = df.loc[gen_params.column_target]
+        test_size = params.get("dev_fraction", 0.)
+        assert (test_size >= 0) and (
+            test_size < 1
+        ), "data_split.dev_fraction must be in [0.0, 1.0)."
+    if params.get("stratify", False):
+        stratify_data = df[general_params.column_target]
     else:
         stratify_data = None
     return train_test_split(
+        df,
         test_size=test_size,
-        random_state=gen_params.prng_seed,
-        shuffle=params.get('shuffle', True),
-        stratify=stratify_data
+        random_state=general_params.prng_seed,
+        shuffle=params.get("shuffle", True),
+        stratify=stratify_data,
     )
--- src/press_start/pipelines/feature_selection/pipeline.py
+++ src/press_start/pipelines/feature_selection/pipeline.py
@@ -1,24 +1,15 @@
 from kedro.pipeline import Pipeline, node
 
-from .nodes import (
-    feature_selection_corr
-)
+from .nodes import feat_selection_corr
 
 
 def create_pipeline(**kwargs):
     return Pipeline([
         node(
-            func=feature_selection_corr,
-            inputs=["dev_dataset", "params:feature_selection"],
+            func=feat_selection_corr,
+            inputs=["dev_dataset", "params:feat_selection_corr", "params:general"],
             outputs=["feat_selection_correlation", "feat_selection_corr_report"],
             name="feat_selection_correlation",
-            tags="feature_selection"
-        ),
-        node(
-            func=feature_selection_corr,
-            inputs=["dev_dataset", "params:feature_selection"],
-            outputs=["feat_selection_rec_elim", "feat_selection_rec_elim_report"],
-            name="feat_selection_recursive_elimination",
             tags="feature_selection"
         ),
     ])
