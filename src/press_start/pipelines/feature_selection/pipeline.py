db2f70798056f348c2d777353f6e0ca94ebbf722
Luiz Otavio Vilas Boas Oliveira <luizvbo@gmail.com>
2022-04-07 18:22:29 +0200

    ENH: Add node to select the K best features

--- src/press_start/pipelines/feature_selection/pipeline.py
+++ src/press_start/pipelines/feature_selection/pipeline.py
@@ -1,15 +1,21 @@
 from kedro.pipeline import Pipeline, node
 
-from .nodes import feat_selection_corr
+from .nodes import feat_selection_k_best
 
 
 def create_pipeline(**kwargs):
-    return Pipeline([
-        node(
-            func=feat_selection_corr,
-            inputs=["dev_dataset", "params:feat_selection_corr", "params:general"],
-            outputs=["feat_selection_correlation", "feat_selection_corr_report"],
-            name="feat_selection_correlation",
-            tags="feature_selection"
-        ),
-    ])
+    return Pipeline(
+        [
+            node(
+                func=feat_selection_k_best,
+                inputs=[
+                    "dev_dataset",
+                    "params:feat_selection_k_best",
+                    "params:general",
+                ],
+                outputs="feat_selection_k_best",
+                name="feat_selection_k_best",
+                tags="feature_selection",
+            ),
+        ]
+    )
